0d0a10f05ebc58ab74c1b1b61b248329724d1c24
Brine <brian@qualityshepherd.com>
2021-02-15 12:49:29 -0700

    words

--- src/views/about.js
+++ src/views/about.js
@@ -3,6 +3,10 @@ const about = {
   render() {
     return `
       <h2>About</h2>
+
+      <div id="about">
+      <a href="https://github.com/qualityshepherd/splog">Splog</a> is a simple, single page blog app written in vanilla js...
+      </div>
     `
   }
 }
