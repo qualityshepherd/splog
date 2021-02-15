4e76c75a6c76467b6464b6bd775539a323c92fb4
Brine <brian@qualityshepherd.com>
2021-02-15 10:18:35 -0700

    missed a meta

--- src/views/post.js
+++ src/views/post.js
@@ -13,7 +13,7 @@ const post = {
         <a href="#post?s=${post.meta.slug}"><h2 class="post-title">${post.meta.title}</h2></a>
         <div class="date">${post.meta.date}</div>
         <div>${post.html}</div>
-        <span class="tags">${renderTags(post.tags)}</span>
+        <span class="tags">${renderTags(post.meta.tags)}</span>
       </div>
       `
   }
