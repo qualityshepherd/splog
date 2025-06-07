2df18a4e10aaa2b72438e1ecd19328d47a992011
Brine <brian@qualityshepherd.com>
2025-06-07 08:54:39 -0600

    add e2e tests!

---
+++ tests/e2e.test.js
@@ -0,0 +1,35 @@
+import test from 'tape'
+import { pptr } from './pptr.js'
+
+const home = `${process.env.TEST_ENV}` || 'https://splog.brine.dev'
+
+test('should display initial post', pptr(async t => {
+  await t.goto(home)
+
+  t.ok(await t.count('.post-title') > 0)
+}))
+
+test('should load more posts', pptr(async t => {
+  await t.goto(home)
+  const initialPostCount = await t.count('.post-title')
+  await t.click('#load-more')
+
+  const after = await t.count('main article')
+  t.ok(await t.count('.post-title') > initialPostCount)
+}))
+
+test('should use menu to navigate to about page', pptr(async t => {
+  await t.goto(home)
+  await t.click('#menu')
+  await t.click('a[href="#about"]')
+
+  t.ok(t.url().includes('#about'))
+}))
+
+test('should search for post', pptr(async t => {
+  await t.goto(home)
+  await t.click('#menu')
+  await t.type('#search', 'human')
+
+  t.ok(await t.count('.post-title') > 0)
+}))
---
+++ tests/pptr.js
@@ -0,0 +1,35 @@
+import puppeteer from 'puppeteer'
+
+export function pptr(testFn) {
+  return async t => {
+    const browser = await puppeteer.launch({ headless: true, slowMo: 0 })
+    const page = await browser.newPage()
+
+    // Query the actual window size using window.outerWidth/outerHeight
+    const windowSize = await page.evaluate(() => ({ width: window.outerWidth, height: window.outerHeight}))
+    await page.setViewport({ width: windowSize.width, height: windowSize.height })
+
+    // Enhance `t` with custom helpers
+    t.page = page
+    t.goto = url => page.goto(url)
+    t.getText = sel => page.$eval(sel, el => el.textContent)
+    t.exists = sel => page.$(sel).then(el => !!el)
+    t.count = sel => page.$$eval(sel, els => els.length)
+    t.click = sel => page.click(sel)
+    t.wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+    t.url = () => page.url()
+    t.type = (sel, text) => page.type(sel, text)
+
+    try {
+      await testFn(t)
+    } catch (err) {
+      await page.screenshot({ path: 'tests/error.png', fullPage: true })
+      const html = await page.content()
+      console.error('Error HTML snapshot:\n', html.slice(0, 1000)) // prevent flooding
+      t.fail(err)
+    } finally {
+      await browser.close()
+      t.end()
+    }
+  }
+}
