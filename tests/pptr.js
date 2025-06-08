2e7bd89fcd2792094dac60a030561a1c0345f6df
Brine <brian@qualityshepherd.com>
2025-06-08 08:14:38 -0600

    words

--- tests/pptr.js
+++ tests/pptr.js
@@ -15,22 +15,30 @@ export function pptr (testFn) {
     await page.setViewport({ width: windowSize.width, height: windowSize.height })
     page.setDefaultTimeout(10000)
 
-    // Enhance `t` with custom helpers
+    // enhance `t` with custom helpers from pptr
     t.page = page
     t.browser = browser
+
+    // nav
     t.goto = url => page.goto(url)
-    t.getText = sel => page.$eval(sel, el => el.textContent)
-    t.exists = sel => page.$(sel).then(el => !!el)
-    t.count = sel => page.$$eval(sel, els => els.length)
-    t.click = sel => page.click(sel)
+    t.url = () => page.url()
     t.wait = ms => new Promise(resolve => setTimeout(resolve, ms))
     t.waitFor = sel => page.waitForSelector(sel)
-    t.url = () => page.url()
+
+    // DOM querying
+    t.exists = sel => page.$(sel).then(el => !!el)
+    t.count = sel => page.$$eval(sel, els => els.length)
+    t.getText = sel => page.$eval(sel, el => el.textContent)
+    t.hasClass = (sel, className) =>
+      page.$eval(sel, (el, cls) => el.classList.contains(cls), className)
+
+    // interaction
+    t.click = sel => page.click(sel)
+    t.clickNth = (selector, index) =>
+      page.$$eval(selector, (elements, i) => { elements[i]?.click() }, index)
     t.type = (sel, text) => page.type(sel, text)
     t.press = key => page.keyboard.press(key)
-    t.hasClass = (sel, className) => page.$eval(sel, (el, cls) => el.classList.contains(cls), className)
-    t.clickNth = (selector, index) => t.page.$$eval(selector, (elements, i) => { elements[i]?.click() }, index)
-    t.eval = fn => page.evaluate(fn) // good for js injection
+    t.eval = fn => page.evaluate(fn)
 
     try {
       await testFn(t)
